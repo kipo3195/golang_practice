623e13d93c964f2d516a5362acd8da34c6ad52bd
kipo3195 <74690413+kipo3195@users.noreply.github.com>
2026-03-03 10:27:29 +0900

    타임아웃이 있는 다중 요청처리기 - 2

--- 2026_03/0303_1/main.go
+++ 2026_03/0303_1/main.go
@@ -15,10 +15,11 @@ func main() {
 	jobs := []string{"job1", "job2", "fail", "job3", "slow_job"}
 
 	// TODO: 1. 500ms 타임아웃 컨텍스트 생성
-	ctx, _ := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
 
-	resultChan := make(chan dto.Result)
-	defer close(resultChan)
+	// 채널은 넣는 로직이 있다면 빼는로직도 반드시 있어야함.
+	resultChan := make(chan dto.Result, len(jobs))
 
 	var wg sync.WaitGroup
 	for i := 0; i < len(jobs); i++ {
@@ -26,7 +27,22 @@ func main() {
 		go worker.Worker(ctx, &wg, jobs[i], resultChan)
 	}
 
-	wg.Wait()
+	// 모든 고루틴이 종료 = 채널 닫기
+	// 기다리고 닫는 역할을 별도 고루틴에 위임 (Non-blocking)
+	go func() {
+		wg.Wait()
+		// 모든 고루틴이 종료되면 wg.Wait이 수행될꺼고.. 그러면 채널이 닫힘 그러니까 아래 for문에서 채널이 닫힐때까지를 명시하는 for range로 처리
+		close(resultChan)
+
+	}()
+
+	for res := range resultChan {
+		log.Printf("결과 수신: %+v", res)
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		log.Println("⚠️ 전체 작업 시간 초과로 일부 작업이 취소되었습니다.")
+	}
 
 	log.Println("작업종료")
 
--- 2026_03/0303_1/worker/worker.go
+++ 2026_03/0303_1/worker/worker.go
@@ -9,26 +9,31 @@ import (
 )
 
 func Worker(ctx context.Context, wg *sync.WaitGroup, jobName string, resultChan chan<- dto.Result) {
+	defer wg.Done() // 함수 종료 시 무조건 실행 보장, for 루프에서는 return 처리만.
+	// 만약 로직이 복잡해져서 다른 곳에서 return 해버리면 Done()이 호출되지 않아 메인이 영원히 기다리게 됩니다.
+
+	// time.Sleep은 Context 취소를 감지하지 못합니다.
+	// 지정된 시간 동안 절대 깨어나지 않습니다. 만약 타임아웃이 500ms이고 슬립이 800ms라면, 500ms 시점에 취소 신호가 와도 무시하고 800ms까지 푹 자버립니다.
+	// select와 time.After를 써야 즉각 반응할 수 있습니다.
+
+	min := 100
+	max := 800
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := r.Intn(max-min+1) + min
+	sleepDuration := time.Duration(n) * time.Millisecond
 
 	select {
 	case <-ctx.Done():
-		wg.Done()
 		return
-	default:
-		min := 100
-		max := 800
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		n := r.Intn(max-min+1) + min
-		time.Sleep(time.Duration(n) * time.Millisecond)
+	case <-time.After(sleepDuration): // 시간이 지난 후 실행 (time.Sleep은 취소시간 감지 불가!!)
 		var err string
 		if jobName == "fail" {
 			err = "error"
-		} else {
-			err = ""
 		}
+
+		// 전송 시점에도 Context 확인 (보내는 도중 취소될 수 있으므로)
 		select {
 		case <-ctx.Done():
-			wg.Done()
 			return
 		case resultChan <- dto.Result{
 			Value: jobName,
